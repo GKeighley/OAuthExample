660238637716c34016d0516931edf8c7f087f608
Glenn Keighley <gkeighley@gmail.com>
2015-09-22 14:02:54 -0700

    Create initial empty swift project

---
+++ OAuthExample/OAuthExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OAuthExample
+//
+//  Created by Glenn Keighley on 9/22/15.
+//  Copyright © 2015 Epocrates. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
